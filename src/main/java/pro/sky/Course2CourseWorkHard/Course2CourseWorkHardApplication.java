07a26dcba469284b24c8552e54a5f0f88efa8b8d
kolia <koliamba_85@mail.ru>
2024-04-05 01:41:55 +0800

    Исходный коммит

---
+++ src/main/java/pro/sky/Course2CourseWorkHard/Course2CourseWorkHardApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2CourseWorkHard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2CourseWorkHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2CourseWorkHardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Course2CourseWorkHard/Course2CourseWorkHardApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2CourseWorkHard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2CourseWorkHardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
